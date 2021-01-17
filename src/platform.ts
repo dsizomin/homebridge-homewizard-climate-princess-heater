f550ab02b64adf1902eaa8b7d27389024ccfea27
Denys Sizomin <denys@miro.com>
2021-01-17 03:01:28 +0100

    Making linter happy.

--- src/platform.ts
+++ src/platform.ts
@@ -5,16 +5,16 @@ import {
   Logger,
   PlatformAccessory,
   PlatformConfig,
-  Service
+  Service,
 } from 'homebridge';
 
 import {PLATFORM_NAME, PLUGIN_NAME} from './settings';
 import {HomewizardPrincessHeaterAccessory} from './platformAccessory';
-import {PrincessHeaterAccessoryContext, ResponseWsIncomingMessage, WsIncomingMessage} from "./ws/types";
-import {DeviceType, MessageType} from "./ws/const";
-import {getDevices, login} from "./http";
-import {open} from "./ws";
-import {WsClient} from "./ws/client";
+import {PrincessHeaterAccessoryContext, ResponseWsIncomingMessage, WsIncomingMessage} from './ws/types';
+import {DeviceType, MessageType} from './ws/const';
+import {getDevices, login} from './http';
+import {open} from './ws';
+import {WsClient} from './ws/client';
 
 /**
  * HomebridgePlatform
@@ -22,126 +22,126 @@ import {WsClient} from "./ws/client";
  * parse the user config and discover/register accessories with Homebridge.
  */
 export class HomebridgePrincessHeaterPlatform implements DynamicPlatformPlugin {
-  public readonly Service: typeof Service = this.api.hap.Service;
-  public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
+    public readonly Service: typeof Service = this.api.hap.Service;
+    public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
-  // this is used to track restored cached accessories
-  public readonly accessories: PlatformAccessory<PrincessHeaterAccessoryContext>[] = [];
+    // this is used to track restored cached accessories
+    public readonly accessories: PlatformAccessory<PrincessHeaterAccessoryContext>[] = [];
 
-  constructor(
-    public readonly log: Logger,
-    public readonly config: PlatformConfig,
-    public readonly api: API,
-  ) {
-    this.log.debug('Finished initializing platform:', this.config.name);
+    constructor(
+        public readonly log: Logger,
+        public readonly config: PlatformConfig,
+        public readonly api: API,
+    ) {
+      this.log.debug('Finished initializing platform:', this.config.name);
 
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
-    this.api.on('didFinishLaunching', () => {
-      log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
-      this.discoverDevices();
-    });
-  }
-
-  /**
-   * This function is invoked when homebridge restores cached accessories from disk at startup.
-   * It should be used to setup event handlers for characteristics and update respective values.
-   */
-  configureAccessory(accessory: PlatformAccessory<PrincessHeaterAccessoryContext>) {
-    this.log.info('Loading accessory from cache:', accessory.displayName);
-
-    // add the restored accessory to the accessories cache so we can track if it has already been registered
-    this.accessories.push(accessory);
-  }
-
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
-  discoverDevices() {
-
-    const authResponsePromise = login(this.config.authorization as string);
-    const wsPromise = open();
-
-    Promise.all([authResponsePromise, wsPromise]).then(([auth, ws]) =>{
-
-      const client = new WsClient(ws)
-
-      ws.on('message', (message: string) => {
-        const incomingMessage: WsIncomingMessage = JSON.parse(message)
-        this.log.debug('Incoming message:', incomingMessage)
-        if (
-            'message_id' in incomingMessage &&
-            incomingMessage.message_id in client.outgoingMessages &&
-            client.outgoingMessages[incomingMessage.message_id].type === MessageType.Hello
-        ) {
-          return this.onHelloMessageResponse(incomingMessage, client);
-        }
+      // When this event is fired it means Homebridge has restored all cached accessories from disk.
+      // Dynamic Platform plugins should only register new accessories after this event was fired,
+      // in order to ensure they weren't added to homebridge already. This event can also be used
+      // to start discovery of new accessories.
+      this.api.on('didFinishLaunching', () => {
+        log.debug('Executed didFinishLaunching callback');
+        // run the method to discover / register your devices as accessories
+        this.discoverDevices();
       });
+    }
 
-      client.send({
-        type: MessageType.Hello,
-        message_id: client.generateMessageId(),
-        version: "2.4.0",
-        os: "ios",
-        source: "climate",
-        compatibility: 3,
-        token: auth.token
-      })
-    })
-  }
-  
-  async onHelloMessageResponse(response: ResponseWsIncomingMessage, wsClient: WsClient) {
-    this.log.debug('Received a response to Hello message. Going to get list of devices...', response);
+    /**
+     * This function is invoked when homebridge restores cached accessories from disk at startup.
+     * It should be used to setup event handlers for characteristics and update respective values.
+     */
+    configureAccessory(accessory: PlatformAccessory<PrincessHeaterAccessoryContext>) {
+      this.log.info('Loading accessory from cache:', accessory.displayName);
 
-    const authorization: string = this.config.authorization as string;
-    const devices = await getDevices(authorization);
+      // add the restored accessory to the accessories cache so we can track if it has already been registered
+      this.accessories.push(accessory);
+    }
 
-    this.log.debug('Received a list of devices:', devices.map(d => d.name));
+    /**
+     * This is an example method showing how to register discovered accessories.
+     * Accessories must only be registered once, previously created accessories
+     * must not be registered again to prevent "duplicate UUID" errors.
+     */
+    discoverDevices() {
 
-    const devicesUUIDs = devices.map(d => this.api.hap.uuid.generate(d.identifier));
+      const authResponsePromise = login(this.config.authorization as string);
+      const wsPromise = open();
 
-    this.accessories
+      Promise.all([authResponsePromise, wsPromise]).then(([auth, ws]) => {
+
+        const client = new WsClient(ws);
+
+        ws.on('message', (message: string) => {
+          const incomingMessage: WsIncomingMessage = JSON.parse(message);
+          this.log.debug('Incoming message:', incomingMessage);
+          if (
+            'message_id' in incomingMessage &&
+                    incomingMessage.message_id in client.outgoingMessages &&
+                    client.outgoingMessages[incomingMessage.message_id].type === MessageType.Hello
+          ) {
+            return this.onHelloMessageResponse(incomingMessage, client);
+          }
+        });
+
+        client.send({
+          type: MessageType.Hello,
+          message_id: client.generateMessageId(),
+          version: '2.4.0',
+          os: 'ios',
+          source: 'climate',
+          compatibility: 3,
+          token: auth.token,
+        });
+      });
+    }
+
+    async onHelloMessageResponse(response: ResponseWsIncomingMessage, wsClient: WsClient) {
+      this.log.debug('Received a response to Hello message. Going to get list of devices...', response);
+
+      const authorization: string = this.config.authorization as string;
+      const devices = await getDevices(authorization);
+
+      this.log.debug('Received a list of devices:', devices.map(d => d.name));
+
+      const devicesUUIDs = devices.map(d => this.api.hap.uuid.generate(d.identifier));
+
+      this.accessories
         .filter(a => !devicesUUIDs.includes(a.UUID))
         .forEach(a => {
           this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [a]);
           this.log.info('Removing existing accessory from cache:', a.displayName);
-        })
+        });
 
-    devices.forEach((device, i) => {
+      devices.forEach((device, i) => {
 
-      if (device.type === DeviceType.Heater) {
+        if (device.type === DeviceType.Heater) {
 
-        const uuid = devicesUUIDs[i];
+          const uuid = devicesUUIDs[i];
 
-        const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+          const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
 
-        if (existingAccessory) {
-          this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-          new HomewizardPrincessHeaterAccessory(this, existingAccessory, wsClient);
-          this.api.updatePlatformAccessories([existingAccessory]);
-        } else {
-          this.log.info('Adding new accessory:', device.name);
+          if (existingAccessory) {
+            this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+            new HomewizardPrincessHeaterAccessory(this, existingAccessory, wsClient);
+            this.api.updatePlatformAccessories([existingAccessory]);
+          } else {
+            this.log.info('Adding new accessory:', device.name);
 
-          const accessory = new this.api.platformAccessory(device.name, uuid);
+            const accessory = new this.api.platformAccessory(device.name, uuid);
 
-          accessory.context.device = device;
+            accessory.context.device = device;
 
-          new HomewizardPrincessHeaterAccessory(
+            new HomewizardPrincessHeaterAccessory(
               this,
-              accessory as PlatformAccessory<PrincessHeaterAccessoryContext>,
-              wsClient
-          );
+                        accessory as PlatformAccessory<PrincessHeaterAccessoryContext>,
+                        wsClient,
+            );
 
-          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+            this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+          }
+        } else {
+          this.log.info('Unsupported device type:', device.type);
         }
-      } else {
-        this.log.info('Unsupported device type:', device.type);
-      }
-    })
-  }
+      });
+    }
 }
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -2,14 +2,14 @@ import {CharacteristicSetCallback, CharacteristicValue, PlatformAccessory, Servi
 
 import {HomebridgePrincessHeaterPlatform} from './platform';
 import {
-    JSONPatchWsIncomingMessage,
-    JSONPatchWsOutgoingMessage,
-    PrincessHeaterAccessoryContext,
-    PrincessHeaterStateWsIncomingMessage,
-    SubscribeWsOutgoingMessage
-} from "./ws/types";
-import {WsClient} from "./ws/client";
-import {MessageType} from "./ws/const";
+  JSONPatchWsIncomingMessage,
+  JSONPatchWsOutgoingMessage,
+  PrincessHeaterAccessoryContext,
+  PrincessHeaterStateWsIncomingMessage,
+  SubscribeWsOutgoingMessage,
+} from './ws/types';
+import {WsClient} from './ws/client';
+import {MessageType} from './ws/const';
 
 /**
  * Platform Accessory
@@ -17,173 +17,175 @@ import {MessageType} from "./ws/const";
  * Each accessory may expose multiple services of different service types.
  */
 export class HomewizardPrincessHeaterAccessory {
-    private service: Service;
+  private service: Service;
 
-    constructor(
-        private readonly platform: HomebridgePrincessHeaterPlatform,
-        private readonly accessory: PlatformAccessory<PrincessHeaterAccessoryContext>,
-        private readonly wsClient: WsClient,
-    ) {
+  constructor(
+    private readonly platform: HomebridgePrincessHeaterPlatform,
+    private readonly accessory: PlatformAccessory<PrincessHeaterAccessoryContext>,
+    private readonly wsClient: WsClient,
+  ) {
 
-        this.service = this.accessory.getService(this.platform.Service.Thermostat) || this.accessory.addService(this.platform.Service.Thermostat);
+    this.service = 
+      this.accessory.getService(this.platform.Service.Thermostat) ||
+      this.accessory.addService(this.platform.Service.Thermostat);
 
-        this.service.setCharacteristic(this.platform.Characteristic.Name, this.accessory.displayName);
+    this.service.setCharacteristic(this.platform.Characteristic.Name, this.accessory.displayName);
 
-        this.service.getCharacteristic(this.platform.Characteristic.TargetHeatingCoolingState)
-            .setProps({
-                validValues: [
-                    this.platform.Characteristic.TargetHeatingCoolingState.OFF,
-                    this.platform.Characteristic.TargetHeatingCoolingState.HEAT
-                ]
-            })
-            .on('set', this.setTargetHeatingCoolingState.bind(this));
-
-
-        this.service.getCharacteristic(this.platform.Characteristic.TargetTemperature)
-            .setProps({
-                unit: Units.CELSIUS,
-                minStep: 1
-            })
-            .on('set', this.setTargetTemperature.bind(this))
-
-        this.service.getCharacteristic(this.platform.Characteristic.LockPhysicalControls)
-            .on('set', this.setLockPhysicalControls.bind(this))
+    this.service.getCharacteristic(this.platform.Characteristic.TargetHeatingCoolingState)
+      .setProps({
+        validValues: [
+          this.platform.Characteristic.TargetHeatingCoolingState.OFF,
+          this.platform.Characteristic.TargetHeatingCoolingState.HEAT,
+        ],
+      })
+      .on('set', this.setTargetHeatingCoolingState.bind(this));
 
 
-        this.wsClient.ws.on('message', this.onWsMessage.bind(this));
+    this.service.getCharacteristic(this.platform.Characteristic.TargetTemperature)
+      .setProps({
+        unit: Units.CELSIUS,
+        minStep: 1,
+      })
+      .on('set', this.setTargetTemperature.bind(this));
 
-        this.platform.log.info('Subscribing to device updates:', this.accessory.context.device.name);
+    this.service.getCharacteristic(this.platform.Characteristic.LockPhysicalControls)
+      .on('set', this.setLockPhysicalControls.bind(this));
 
-        const message: SubscribeWsOutgoingMessage = {
-            type: MessageType.SubscribeDevice,
-            device: this.accessory.context.device.identifier,
-            message_id: wsClient.generateMessageId()
+
+    this.wsClient.ws.on('message', this.onWsMessage.bind(this));
+
+    this.platform.log.info('Subscribing to device updates:', this.accessory.context.device.name);
+
+    const message: SubscribeWsOutgoingMessage = {
+      type: MessageType.SubscribeDevice,
+      device: this.accessory.context.device.identifier,
+      message_id: wsClient.generateMessageId(),
+    };
+
+    wsClient.send(message);
+  }
+
+  onWsMessage(message: string) {
+    const incomingMessage = JSON.parse(message);
+    if ('state' in incomingMessage) {
+      this.onStateMessage(incomingMessage);
+    } else if ('type' in incomingMessage && incomingMessage.type === MessageType.JSONPatch) {
+      this.onJSONPatchMessage(incomingMessage);
+    }
+  }
+
+  onJSONPatchMessage(message: JSONPatchWsIncomingMessage) {
+    message.patch.forEach(patchItem => {
+      const {op, path, value} = patchItem;
+
+      if (op === 'replace') {
+        const match = path.match(/^\/state\/(.*)$/);
+        if (match && match[1]) {
+          const stateKey = match[1];
+
+          switch (stateKey) {
+            case 'power_on':
+              this.service.updateCharacteristic(
+                this.platform.Characteristic.CurrentHeatingCoolingState,
+                value ?
+                  this.platform.Characteristic.CurrentHeatingCoolingState.HEAT :
+                  this.platform.Characteristic.CurrentHeatingCoolingState.OFF,
+              );
+              break;
+            case 'current_temperature':
+              this.service.updateCharacteristic(
+                this.platform.Characteristic.CurrentTemperature,
+                value,
+              );
+              break;
+          }
         }
+      }
+    });
+  }
 
-        wsClient.send(message)
-    }
+  onStateMessage(message: PrincessHeaterStateWsIncomingMessage) {
+    this.platform.log.info('Updating state from message ->', message);
 
-    onWsMessage(message: string) {
-        const incomingMessage = JSON.parse(message)
-        if ('state' in incomingMessage) {
-            this.onStateMessage(incomingMessage)
-        } else if ('type' in incomingMessage && incomingMessage.type === MessageType.JSONPatch) {
-            this.onJSONPatchMessage(incomingMessage)
-        }
-    }
+    Object.keys(message.state).forEach(key => {
+      const value = message.state[key];
 
-    onJSONPatchMessage(message: JSONPatchWsIncomingMessage) {
-        message.patch.forEach(patchItem => {
-            const {op, path, value} = patchItem
+      switch (key) {
+        case 'power_on':
+          this.service.setCharacteristic(
+            this.platform.Characteristic.CurrentHeatingCoolingState,
+            value ?
+              this.platform.Characteristic.CurrentHeatingCoolingState.HEAT :
+              this.platform.Characteristic.CurrentHeatingCoolingState.OFF,
+          );
+          break;
+        case 'current_temperature':
+          this.service.setCharacteristic(
+            this.platform.Characteristic.CurrentTemperature,
+            value,
+          );
+          break;
+      }
+    });
+  }
 
-            if (op === 'replace') {
-                const match = path.match(/^\/state\/(.*)$/)
-                if (match && match[1]) {
-                    const stateKey = match[1];
+  setTargetHeatingCoolingState(value: CharacteristicValue, callback: CharacteristicSetCallback) {
 
-                    switch (stateKey) {
-                        case 'power_on':
-                            this.service.updateCharacteristic(
-                                this.platform.Characteristic.CurrentHeatingCoolingState,
-                                value ?
-                                    this.platform.Characteristic.CurrentHeatingCoolingState.HEAT :
-                                    this.platform.Characteristic.CurrentHeatingCoolingState.OFF
-                            );
-                            break;
-                        case 'current_temperature':
-                            this.service.updateCharacteristic(
-                                this.platform.Characteristic.CurrentTemperature,
-                                value
-                            );
-                            break;
-                    }
-                }
-            }
-        })
-    }
+    this.platform.log.debug('Set Characteristic TargetHeatingCoolingState ->', value);
 
-    onStateMessage(message: PrincessHeaterStateWsIncomingMessage) {
-        this.platform.log.info('Updating state from message ->', message);
+    const message: JSONPatchWsOutgoingMessage = {
+      type: MessageType.JSONPatch,
+      message_id: this.wsClient.generateMessageId(),
+      device: this.accessory.context.device.identifier,
+      patch: [{
+        op: 'replace',
+        path: '/state/power_on',
+        value: value === this.platform.Characteristic.TargetHeatingCoolingState.HEAT,
+      }],
+    };
 
-        Object.keys(message.state).forEach(key => {
-            const value = message.state[key]
+    this.wsClient.send(message);
 
-            switch (key) {
-                case 'power_on':
-                    this.service.setCharacteristic(
-                        this.platform.Characteristic.CurrentHeatingCoolingState,
-                        value ?
-                            this.platform.Characteristic.CurrentHeatingCoolingState.HEAT :
-                            this.platform.Characteristic.CurrentHeatingCoolingState.OFF
-                    );
-                    break;
-                case 'current_temperature':
-                    this.service.setCharacteristic(
-                        this.platform.Characteristic.CurrentTemperature,
-                        value
-                    );
-                    break;
-            }
-        })
-    }
+    callback(null);
+  }
 
-    setTargetHeatingCoolingState(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+  setTargetTemperature(value: CharacteristicValue, callback: CharacteristicSetCallback) {
 
-        this.platform.log.debug('Set Characteristic TargetHeatingCoolingState ->', value);
+    const message: JSONPatchWsOutgoingMessage = {
+      type: MessageType.JSONPatch,
+      message_id: this.wsClient.generateMessageId(),
+      device: this.accessory.context.device.identifier,
+      patch: [{
+        op: 'replace',
+        path: '/state/target_temperature',
+        value: value,
+      }],
+    };
 
-        const message: JSONPatchWsOutgoingMessage = {
-            type: MessageType.JSONPatch,
-            message_id: this.wsClient.generateMessageId(),
-            device: this.accessory.context.device.identifier,
-            patch: [{
-                op: 'replace',
-                path: '/state/power_on',
-                value: value === this.platform.Characteristic.TargetHeatingCoolingState.HEAT
-            }]
-        }
+    this.platform.log.debug('Set Characteristic TargetTemperature ->', value);
 
-        this.wsClient.send(message);
+    this.wsClient.send(message);
 
-        callback(null)
-    }
+    callback(null);
+  }
 
-    setTargetTemperature(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+  setLockPhysicalControls(value: CharacteristicValue, callback: CharacteristicSetCallback) {
 
-        const message: JSONPatchWsOutgoingMessage = {
-            type: MessageType.JSONPatch,
-            message_id: this.wsClient.generateMessageId(),
-            device: this.accessory.context.device.identifier,
-            patch: [{
-                op: 'replace',
-                path: '/state/target_temperature',
-                value: value
-            }]
-        }
+    const message: JSONPatchWsOutgoingMessage = {
+      type: MessageType.JSONPatch,
+      message_id: this.wsClient.generateMessageId(),
+      device: this.accessory.context.device.identifier,
+      patch: [{
+        op: 'replace',
+        path: '/state/lock',
+        value: Boolean(value),
+      }],
+    };
 
-        this.platform.log.debug('Set Characteristic TargetTemperature ->', value);
+    this.platform.log.debug('Set Characteristic LockPhysicalControls ->', value);
 
-        this.wsClient.send(message);
+    this.wsClient.send(message);
 
-        callback(null)
-    }
-
-    setLockPhysicalControls(value: CharacteristicValue, callback: CharacteristicSetCallback) {
-
-        const message: JSONPatchWsOutgoingMessage = {
-            type: MessageType.JSONPatch,
-            message_id: this.wsClient.generateMessageId(),
-            device: this.accessory.context.device.identifier,
-            patch: [{
-                op: 'replace',
-                path: '/state/lock',
-                value: Boolean(value)
-            }]
-        }
-
-        this.platform.log.debug('Set Characteristic LockPhysicalControls ->', value);
-
-        this.wsClient.send(message);
-
-        callback(null)
-    }
+    callback(null);
+  }
 }
